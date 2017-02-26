9cae2500cc731019d731940e926bea466647fb8d
huayang <764220199@qq.com>
2017-02-26 16:17:53 +0800

    齐薇薇的

---
+++ src/main/java/hellWorld2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017-02-26.
+ */
+public class hellWorld2 {
+    public static void main(String[] args){
+        System.out.println("hello world 2!!!");
+    }
+}

2ccec9e351161ec80b8fbf2aadd0672a5750d896
huayang <764220199@qq.com>
2017-02-26 16:22:42 +0800

    齐薇薇的

--- src/main/java/hellWorld2.java
+++ src/main/java/hellWorld2.java
@@ -5,5 +5,6 @@ public class hellWorld2 {
     public static void main(String[] args){
         System.out.println("hello world 2!!!");
         System.out.println("ni hao");
+        System.out.println("ni hao");
     }
 }
